5e3598772146f370738f334dea065d7cf1dce344
pxp223 <peter.parianos@sherwin.com>
2020-02-29 19:01:53 -0500

    tab fix

--- project 2/project2.py
+++ project 2/project2.py
@@ -1,15 +1,25 @@
 import csv
 import json
 import pandas as pf
-
+from pandas.io.json import json_normalize
 
 rows = list()
+
 with open("business100ValidForm.json") as json_file:
     file1 = json.load(json_file)
     data = dict(file1)
     rows = data['Business']
 
-df = pf.DataFrame(rows)
+
+df = json_normalize(rows)
+# df = df.astype(str)
+
 print(df)
-with open('./file.csv', 'w') as csv_f:
-    df.to_csv(csv_f)
+
+with open('./file.tsv', 'w') as csv_f:
+    df.to_csv(csv_f, sep='\t', index=False)
+
+
+
+#use pandas to normalize data
+
