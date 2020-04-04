6a33a630852da4001c7e93d2a1e1c4c0438ecf67
pxp223 <peter.parianos@sherwin.com>
2020-04-04 16:29:40 -0400

    got bitcoin tweets

--- project_3/twitter.py
+++ project_3/twitter.py
@@ -4,24 +4,24 @@ from tweepy.streaming import StreamListener #For streaming tweets
 import json #For writing to a JSON file
 
 # Credentials that are generated in the Twitter developer app
-access_token = '837612670391988226-cOGK8kOF9xui3plGfaVtFdLgdEqoAK6'
-access_token_secret = 'FQNFcWL4OfQ2BRp5KsIIXJD0CWJOY3aoGomtrmmMuYkmC'
-consumer_token = 'xBeyAlAUemzXJZ1FDLgH4rZCp'
-consumer_token_secret = 'oRElpAezmEwFctz0ASmuSDGUT8Tx06h1psFteeX7lqiF3rY1NP'
+access_token = '4499120554-xlLcrpreHvGvBgs99IKYGwme7w7tfmwPWbEYM5X'
+access_token_secret = 'UfoVVeJBD6klcPOkYOBnC0uiQM4hV7YWX7OfoEYfRihtZ'
+consumer_token = 'cHBdb7LKMSPTQyfGJ2GjxuysX'
+consumer_token_secret = 'n23SRANIVjqBG7ol3NHPvfWYEFLpIK9dA39FXcSTziEZNehfot'
 
 class Listen(StreamListener):
     def on_status (self, status):
         json_str = json.dumps(status._json)
         print(json_str) #printing the Json string that is being fetched...
         try:
-            with open("coronatweets_11_53.json","a") as file: #appending the JSON
+            with open("bitcointweets.json","a") as file: #appending the JSON
             #data to the file
                 file.write(json_str+",\n")
         except:
             pass
         return True
     def on_error(self, status_code):
-        print(status_code)
+        print("Exception has occurred, here is the access code: ", status_code)
         if status_code == 420:
             #returning False in on_error disconnects the stream
             return False        
@@ -33,5 +33,6 @@ if __name__ == "__main__":
     authentication = OAuthHandler(consumer_token,consumer_token_secret)
     authentication.set_access_token(access_token, access_token_secret)
     stream = Stream(authentication, listener) #stream object with authentication
-    stream.filter(follow = ['1242461488406843393']) #filtering the tweets with 
-    listener.on_status()
+    #use follow in order to track an ID
+    list_of_search_terms = ['bitcoin', 'cryptocurrencies', 'Ethereum']
+    stream.filter(track = list_of_search_terms)
